1cc7006a9e0cd9f97445567fff9a4cf59c6543de
Gevorg <narodniyplusmagazin@gmail.com>
2026-02-13 13:42:04 +0400

    fix: remove pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
-// Register service worker for PWA functionality
-
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
